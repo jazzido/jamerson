675aa6f2502eb13b72ca551633bf82b33196ca8c
Manuel Aristaran <jazzido@jazzido.com>
2017-05-16 19:37:19 -0400

    bleh

--- src/Chart.js
+++ src/Chart.js
@@ -2,48 +2,112 @@ import * as d3 from "d3";
 
 export default class {
 
-    constructor(data) {
+    constructor(data, midiHeader) {
         this.data = data;
+
+        this.msBeat = this.data
+            .filter(d => d[0].event.subtype === 'setTempo')[0][0]
+            .event.microsecondsPerBeat;
+
+        const kSecondsPerQuarterNote = this.msBeat / 1000000.0;
+        this.kSecondsPerTick = kSecondsPerQuarterNote / midiHeader.ticksPerBeat;
+
         this.noteExtent = d3.extent(this.data
                                     .filter((d) => d[0].event.subtype === "noteOn")
                                     .map((d) => d[0].event.noteNumber));
+
+        this.velocityExtent = d3.extent(this.data
+                                        .filter((d) => d[0].event.subtype === "noteOn")
+                                        .map((d) => d[0].event.velocity));
+
 
         this.createPoints();
         this.initChart();
     }
 
     initChart() {
-        var svg = d3.select("#chart").append("svg");
-        console.log(svg);
-        var margin = {top: 10, right: 30, bottom: 30, left: 30},
-            width = +svg.attr("width") - margin.left - margin.right,
-            height = +svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        this.svg = d3.select("#chart").append("svg");
+        window.svg = this.svg;
+
+        const svgSize = this.svg.node().getBoundingClientRect();
+
+        var margin = {top: 10, right: 30, bottom: 30, left: 30};
+
+        this.yScale = d3.scaleLinear()
+            .domain(this.noteExtent)
+            .range([svgSize.height - margin.bottom, margin.top]);
+        this.xScale = d3.scaleLinear()
+            .domain([0, this.noteInfos[this.noteInfos.length - 1].startTime])
+            .range([margin.left, svgSize.width - margin.right]);
+
+        this.svg.selectAll("circle")
+            .data(this.noteInfos)
+            .enter()
+            .append('circle')
+            .attr('r', 10)
+            .attr('cx', (d) => this.xScale(d.startTime))
+            .attr('cy', (d) => this.yScale(d.noteNumber));
+
+        this.svg.selectAll('circle.currentNote')
+            .data([this.noteInfos[0]])
+            .enter()
+            .append('circle')
+            .attr('r', 10)
+            .attr('cx', (d) => this.xScale(d.startTime))
+            .attr('cy', (d) => this.yScale(d.noteNumber))
+            .attr('class', 'currentNote');
     }
 
     createPoints() {
         var currentTime = 0;
         this.noteInfos = [];
-        this.noteTimes = [];
+        var noteTimes = [];
 
         // thanks euphony
         // https://github.com/qiao/euphony/blob/master/src/coffee/NoteRain.coffee
         this.data.forEach(([event, interval]) => {
             currentTime += interval;
+            //currentTime += event.event.deltaTime;
             var {subtype, noteNumber, channel} = event.event;
 
             if (subtype === 'noteOn') {
-                this.noteTimes[noteNumber] = currentTime;
-            }
-            else if (subtype === 'noteOff') {
-                var startTime = this.noteTimes[noteNumber];
-                var duration = currentTime - startTime;
                 this.noteInfos.push({
                     noteNumber: noteNumber,
-                    startTime: startTime,
-                    duration: duration
+                    startTime: currentTime
                 });
+                //noteTimes[noteNumber] = currentTime;
             }
+            // else if (subtype === 'noteOff') {
+            //     var startTime = noteTimes[noteNumber];
+            //     var duration = currentTime - startTime;
+            //     this.noteInfos.push({
+            //         noteNumber: noteNumber,
+            //         startTime: startTime,
+            //         duration: duration
+            //     });
+            // }
         });
     }
+
+    update(data, note_idx) {
+        const ticksToNext = this.noteInfos[note_idx+1].startTime - this.noteInfos[note_idx].startTime;
+
+        const t = d3.transition();
+
+        // console.log(ticksToNext * this.kSecondsPerTick);
+
+        this.svg.selectAll('circle.currentNote')
+            .data([this.noteInfos[note_idx+1]])
+            .transition(d3.transition())
+            .attr('cx', (d) => this.xScale(d.startTime))
+            .attr('cy', (d) => this.yScale(d.noteNumber))
+            .attr('r', 0)
+            .transition(d3.transition().duration(10))
+            .attr('r', 10);
+
+        this.svg.select(`circle:not(.currentNote):nth-child(${note_idx+1})`)
+            .attr('class', 'visited');
+
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -17,39 +17,47 @@ MIDI.loadPlugin({
             delay = 0.5; // how long to hold the note, in seconds
         // play the note
         MIDI.programChange(0, instrument); // Load xylophone into Channel 0
-        MIDI.noteOn(0, note, velocity) // Play middle C on Channel 0
-        MIDI.noteOff(0, note, delay) // Release the middle C after 0.5 seconds
+        MIDI.noteOn(0, note, velocity); // Play middle C on Channel 0
+        MIDI.noteOff(0, note, delay); // Release the middle C after 0.5 seconds
     }
 });
 
-/*
-MIDI.Player.addListener(function(data) { // set it to your own function!
-    var now = data.now; // where we are now
-    var end = data.end; // time when song ends
-    var channel = data.channel; // channel note is playing on
-    var message = data.message; // 128 is noteOff, 144 is noteOn
-    var note = data.note; // the note
-    var velocity = data.velocity; // the velocity of the note
-    // then do whatever you want with the information!
-    //console.log(now, end, channel, message, note, velocity);
-});
-*/
 
-/*
+const NOTE_ON = 144;
+
 MIDI.Player.clearAnimation(); // clears current animation.
 MIDI.Player.setAnimation(function(data) {
-    var now = data.now; // where we are now
-    var end = data.end; // time when song ends
-    var events = data.events; // all the notes currently being processed
-    console.log('animation', data);
+    // var now = data.now; // where we are now
+    // var end = data.end; // time when song ends
+    // var events = data.events; // all the notes currently being processed
+    //console.log('animation', data);
+//    console.log(data);
+  //  if (data.event.message === NOTE_ON) {
+    //    console.log('animation', data);
+    //}
 });
-*/
+
+var note_idx = 0;
 
 function play() {
-    console.log('player', MIDI.Player.data);
-    var chart = new Chart(MIDI.Player.data);
-    console.log('chart', chart);
+    var chart = new Chart(MIDI.Player.data, MidiFile(MIDI.Player.currentData).header);
+    
+    console.log('play this', this, MIDI);
+    MIDI.Player.addListener(function(data) { // set it to your own function!
+        var now = data.now; // where we are now
+        var end = data.end; // time when song ends
+        var channel = data.channel; // channel note is playing on
+        var message = data.message; // 128 is noteOff, 144 is noteOn
+        var note = data.note; // the note
+        var velocity = data.velocity; // the velocity of the note
+        // then do whatever you want with the information!
+        //console.log(now, end, channel, message, note, velocity);
+        if (data.message === NOTE_ON) {
+            chart.update(data, note_idx++);
+        }
+    });
+
     MIDI.Player.start();
 }
-
+MIDI.Player.BPM = null;
 MIDI.Player.loadFile('/static/midi/Textures2-Bass.mid', play, () => {}, (e) => console.log('error', e));
